98cec7c10cd71793dfa8fb68a1551ba5dc566e99
Xinzhuo Liu <xinzhuo.liu@afusion.com>
2019-05-13 18:35:44 +0800

    20190513

---
+++ src/S174.java
@@ -0,0 +1,23 @@
+import Util.ListNode;
+
+public class S174 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // write your code here
+        ListNode fast = head;
+        ListNode slow = head;
+        for (int i = 0; i < n ; i++) {
+            fast = fast.next;
+        }
+
+        if (fast == null)
+            return head.next;
+
+        while (fast.next != null)
+        {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
---
+++ src/S374.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class S374 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        // write your code here
+        List<Integer> result = new ArrayList<Integer>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length==0)
+            return result;
+        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+        int row = matrix.length;
+        int col = matrix[0].length;
+        boolean[][] visited = new boolean[row][col];
+        int r = 0;
+        int c = 0;
+        int di = 0;
+        for (int i = 0; i < row * col; i++) {
+            result.add(matrix[r][c]);
+            visited[r][c] = true;
+            int cr = r + dirs[di][0];
+            int cc = c + dirs[di][1];
+            if (0 <= cr && cr < row && 0 <= cc && cc < col && !visited[cr][cc]){
+                r = cr;
+                c = cc;
+            } else {
+                di = (di + 1) % 4;
+                r += dirs[di][0];
+                c += dirs[di][1];
+            }
+        }
+        return result;
+    }
+}
